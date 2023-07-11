0393ffec0830607eab9003a4c7e6dd042dda846b
Mr.D3ADL9CK <118673693+DarkDeadLock@users.noreply.github.com>
2023-07-11 22:38:52 +0530

    Create index.js
    This is firstone

---
+++ index.js
@@ -0,0 +1,10 @@
+var uName = promt("hello,what is your name...?");
+console.log("Hello"+uName+".Welcome To The System");
+alert("Good Bye");
+var myName = "Dinux Diveloper"
+var myAge = 18;
+var Job = "programmer";
+console.log(myAge++);
+console.log("hello...I am" + myName + ".I am" + myAge + "years old.I am a " + Job);
+console.log(typeof(myName));
+
